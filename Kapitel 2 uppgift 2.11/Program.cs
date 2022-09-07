389d9f0a2b0f5f31800e5fef6626311cc89e0715
EmilWallett <nhlpool27@gmail.com>
2022-09-07 12:07:49 +0200

    Add project files.

---
+++ Kapitel 2 uppgift 2.11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin och Alam?");
+            Console.WriteLine("Hur långt hoppade Elin i meter?"); string Elin = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma i meter?"); string Alma = Console.ReadLine();
+
+            float Elin_längd_tal = float.Parse(Elin);
+            float Alma_längd_tal = float.Parse(Alma);
+
+            float Skilland = Elin_längd_tal - Alma_längd_tal; 
+
+            Console.WriteLine("Elin hoppade" + Skilland + "Längre en Alma ");
+        }
+    }
+
+
+}
+
